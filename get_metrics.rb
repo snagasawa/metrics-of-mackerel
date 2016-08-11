d0295e4500d2e8aa5e26bf4e979bba67a8b7377e
snagasawa <whitecrane8920@gmail.com>
2016-08-11 22:28:24 +0900

    Get a organization's name

--- get_metrics.rb
+++ get_metrics.rb
@@ -1,1 +1,45 @@
 #!/bin/ruby
+require 'net/http'
+require 'json'
+
+MACKEREL_API_LOCATION = 'https://mackerel.io/api/v0'.freeze
+
+class API
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  def get_json(url)
+    request = Net::HTTP::Get.new(url.path)
+    request['X-Api-Key'] = @api_key
+    request['Content-Type'] = 'application/json'
+
+    response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
+      http.open_timeout = 5
+      http.read_timeout = 10
+      http.request(request)
+    end
+    case response
+    when Net::HTTPSuccess
+      json = response.body
+      JSON.parse(json)
+    else
+      puts response.value
+      exit
+    end
+  rescue => e
+    puts [e.class, e].join("\n")
+    exit
+  end
+end
+
+def organization_url
+  URI.parse("#{MACKEREL_API_LOCATION}/org")
+end
+
+print 'Api Key: '
+api = API.new(STDIN.gets.chomp)
+
+organization = api.get_json(organization_url)
+puts "Organization:"
+puts "  name: #{organization['name']}"
