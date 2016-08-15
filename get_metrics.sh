6a1d3b8c35af87a29b2791a61f132bbc7d3115ca
snagasawa <whitecrane8920@gmail.com>
2016-08-15 14:24:20 +0900

    Get metric names

--- get_metrics.sh
+++ get_metrics.sh
@@ -16,3 +16,13 @@ hosts_url='https://mackerel.io/api/v0/hosts'
 hosts=`get_json ${api_key} ${hosts_url}`
 echo "\nHosts: "
 echo ${hosts} | jq -r '.hosts[] | .name'
+
+# Get metric names
+printf "\nHost Name: "
+read host_name
+echo ${host_name}
+host_id=`echo ${hosts} | jq -r --arg host_name ${host_name} '.hosts[] | if .name == $host_name then .id else empty end'`
+metric_names_url="https://mackerel.io/api/v0/hosts/${host_id}/metric-names"
+metrics=`get_json ${api_key} ${metric_names_url}`
+echo "\nMetrics: "
+echo ${metrics} | jq -r '.names[]'
