83209321bffe168205ae77ac3dffb957dc500346
snagasawa <whitecrane8920@gmail.com>
2016-08-16 03:05:26 +0900

    wip

--- get_metrics.sh
+++ get_metrics.sh
@@ -28,7 +28,7 @@ echo "\nMetrics: "
 echo $metrics | jq -r '.names[]'
 
 # Get metric values
-printf "\nMetric Name: "
+printf "\nMetric Name (or all): "
 read metric_name
 printf "First Day(YYYYMMDD): "
 read first_day
@@ -40,14 +40,24 @@ last_day=$(date -j -f %Y%m%d $last_day +%s)
 # メソッドで呼び出すと、なぜか400 bad requestになり値が取れない
 # metric_url="https://mackerel.io/api/v0/hosts/{$host_id}/metrics\?name\=${metric_name}\&from\=${first_day}\&to\=${last_day}"
 # metric_values=`get_json ${api_key} ${metric_url}`
-metric_values=$(curl -s -X GET -H "X-Api-Key: $api_key" https://mackerel.io/api/v0/hosts/$host_id/metrics\?name\=$metric_name\&from\=$first_day\&to\=$last_day)
-sum=$(echo $metric_values | jq '.metrics | map(.value) | add')
-count=$(echo $metric_values | jq '.metrics | length')
-max=$(echo $metric_values | jq '.metrics | map(.value) | max')
-average=$(echo $metric_values | jq '.metrics | map(.value) | length as $metric_length | add / $metric_length')
-from_time=$(date -r $(echo $metric_values | jq '.metrics[0] | .time') +"%Y/%m/%d %H:%M:%S")
-to_time=$(date -r $(echo $metric_values | jq '.metrics[-1] | .time') +"%Y/%m/%d %H:%M:%S")
+if [ $metric_name == 'all' ]; then
+    metric_names=$(echo $metrics | jq -r '.names[]')
+    converted_host_name=$(echo $host_name | sed -e 's/\./_/g')
+    file_name="${converted_host_name}_$(date +%s).csv"
+    echo 'MetricName,Average,Max,From,To,Sum,Count' >> $file_name
 
+    for metric_name in $metric_names[@]; do
+        metric_values=$(curl -s -X GET -H "X-Api-Key: $api_key" https://mackerel.io/api/v0/hosts/$host_id/metrics\?name\=$metric_name\&from\=$first_day\&to\=$last_day)
+        sum=$(echo $metric_values | jq '.metrics | map(.value) | add')
+        count=$(echo $metric_values | jq '.metrics | length')
+        max=$(echo $metric_values | jq '.metrics | map(.value) | max')
+        average=$(echo $metric_values | jq '.metrics | map(.value) | length as $metric_length | add / $metric_length')
+        from_time=$(date -r $(echo $metric_values | jq '.metrics[0] | .time') +"%Y/%m/%d %H:%M:%S")
+        to_time=$(date -r $(echo $metric_values | jq '.metrics[-1] | .time') +"%Y/%m/%d %H:%M:%S")
+        # Export to CSV
+        echo "$metric_name,$average,$max,$from_time,$to_time,$sum,$count" >> $file_name
+    done
+else
 cat << EOS
 
 Results:
@@ -65,6 +75,7 @@ converted_host_name=$(echo $host_name | sed -e 's/\./_/g')
 file_name="${converted_host_name}_$(date +%s).csv"
 echo 'MetricName,Average,Max,From,To,Sum,Count' >> $file_name
 echo "$metric_name,$average,$max,$from_time,$to_time,$sum,$count" >> $file_name
+fi
 
 if [ -e $file_name ]; then
     echo "\nExported results to $file_name."
