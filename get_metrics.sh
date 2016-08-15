ceb3a78c4567b8453c3982f4bb2802de78a7d9d1
snagasawa <whitecrane8920@gmail.com>
2016-08-15 12:52:33 +0900

    Get hosts

--- get_metrics.sh
+++ get_metrics.sh
@@ -10,3 +10,9 @@ organization_url='https://mackerel.io/api/v0/org'
 organization=`get_json ${api_key} ${organization_url}`
 printf 'Organization Name: '
 echo ${organization} | jq -r .name
+
+# Get hosts
+hosts_url='https://mackerel.io/api/v0/hosts'
+hosts=`get_json ${api_key} ${hosts_url}`
+echo "\nHosts: "
+echo ${hosts} | jq -r '.hosts[] | .name'
